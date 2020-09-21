3833e1b52faeb8cda40ebc24874bd8eea8c9e4f1
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-09-21 13:54:39 -0300

    feat: próximo agendamento

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Calendar,
+  Container,
+  Content,
+  Header,
+  HeaderContent,
+  NextAppointment,
+  Profile,
+  Schedule,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../context/AuthContext';
@@ -33,6 +42,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 6</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://api.adorable.io/avatars/285/abott@adorable.png"
+                alt="avatars"
+              />
+              <strong>UserName</strong>
+              <span>
+                <FiClock />
+                09:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+
+        <Calendar>asdf</Calendar>
+      </Content>
     </Container>
   );
 };
