62accf28020e2e34b6a96103448668fc59167770
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-09-23 11:18:49 -0300

    feat: página de perfil

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,135 @@
+import React, { useCallback, useRef, useMemo } from 'react';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import { FiMail, FiLock, FiUser, FiCamera, FiArrowLeft } from 'react-icons/fi';
+import * as Yup from 'yup';
+
+import { useHistory, Link } from 'react-router-dom';
+import { AvatarInput, Container, Content } from './styles';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import getValidationErrors from '../../utils/getValidationErrors';
+import api from '../../services/api';
+
+import { useToast } from '../../context/ToastContext';
+import { useAuth } from '../../context/AuthContext';
+
+interface IProfileFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const Profile: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
+  const { user } = useAuth();
+
+  const selectedAvatar = useMemo(() => {
+    return (
+      user.avatar_url || `https://api.adorable.io/avatars/186/${user.name}.png`
+    );
+  }, [user]);
+
+  const handleSubmit = useCallback(
+    async (data: IProfileFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string().min(6, 'Mínimo de seis digitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await api.post('/users', data);
+
+        history.push('/');
+
+        addToast({
+          type: 'success',
+          title: 'Cadastro realizado!',
+          description: 'Você já pode fazer seu logon no GoBarber!',
+        });
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+          formRef.current?.setErrors(errors);
+          return;
+        }
+        addToast({
+          type: 'error',
+          title: 'Erro na cadastro',
+          description:
+            'Ocorreu um erro durante o cadastro, cheque os dados fornecidos',
+        });
+      }
+    },
+    [addToast, history],
+  );
+
+  return (
+    <Container>
+      <header>
+        <div>
+          <Link to="/dashboard">
+            <FiArrowLeft />
+          </Link>
+        </div>
+      </header>
+      <Content>
+        <Form
+          ref={formRef}
+          initialData={{
+            name: user.name,
+            email: user.email,
+          }}
+          onSubmit={handleSubmit}
+        >
+          <AvatarInput>
+            <img src={selectedAvatar} alt={user.name} />
+            <button type="button">
+              <FiCamera />
+            </button>
+          </AvatarInput>
+
+          <h1>Meu Perfil</h1>
+
+          <Input icon={FiUser} name="name" placeholder="Nome" />
+          <Input icon={FiMail} name="email" placeholder="E-mail" />
+
+          <Input
+            containerStyle={{ marginTop: 24 }}
+            icon={FiLock}
+            name="old_password"
+            type="password"
+            placeholder="Senha atual"
+          />
+
+          <Input
+            icon={FiLock}
+            name="password"
+            type="password"
+            placeholder="Nova senha"
+          />
+
+          <Input
+            icon={FiLock}
+            name="password_confirmation"
+            type="password"
+            placeholder="Confirmar senha"
+          />
+          <Button type="submit">Confirmar mudanças</Button>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
+
+export default Profile;
---
+++ src/pages/Profile/styles.ts
@@ -0,0 +1,103 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.div`
+  > header {
+    height: 9rem;
+    background-color: #28262e;
+
+    display: flex;
+    align-items: center;
+
+    div {
+      width: 100%;
+      max-width: 1120px;
+      margin: 0 auto;
+
+      svg {
+        color: #999591;
+        width: 25px;
+        height: 25px;
+      }
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: -160px auto 0;
+
+  width: 100%;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+      font-size: 1.25rem;
+      margin-bottom: 1.5rem;
+      text-align: left;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+`;
+
+export const AvatarInput = styled.div`
+  margin-bottom: 2rem;
+  position: relative;
+  width: 11.625rem;
+  align-self: center;
+
+  img {
+    width: 11.625rem;
+    height: 11.625rem;
+    border-radius: 100%;
+  }
+
+  button {
+    position: absolute;
+    width: 3rem;
+    height: 3rem;
+    background-color: #ff9000;
+
+    border-radius: 100%;
+    border: none;
+
+    bottom: 0;
+    right: 0;
+
+    transition: background-color 0.2s;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      width: 1.25rem;
+      height: 1.25rem;
+      color: #312e38;
+    }
+
+    &:hover {
+      background: ${shade(0.2, '#ff9000')};
+    }
+  }
+`;
